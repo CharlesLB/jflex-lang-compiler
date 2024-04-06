42b56a9130229ef83bbdc6a4416e67dc2c77b9c7
charles-braga-unvoid <165035397+charles-braga-unvoid@users.noreply.github.com>
2024-04-06 16:56:42 -0300

    feat: readFile

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,29 @@
+import java.io.IOException;
+
 import core.LexerAnalyzer.LexerAnalyzer;
+import utils.Reader.Reader;
 
 public class App {
     public static void main(String[] args) {
-        LexerAnalyzer lexer = new LexerAnalyzer();
+        try {
+            LexerAnalyzer lexer = new LexerAnalyzer();
+            String data = getFileData();
 
+            System.out.println(data);
+            // lexer.tokenize(data);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    private static String getFileData() throws IOException {
+        try {
+            String expr = Reader.readFile("src/main/resources/samples/sample1.txt");
+            return expr;
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
 }
