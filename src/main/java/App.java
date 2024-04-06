6cf3bb82fa92a9b22a34fae5cc6c8f72ac9d84c2
charles-braga-unvoid <165035397+charles-braga-unvoid@users.noreply.github.com>
2024-04-06 19:49:34 -0300

    chore: bash mode

--- src/main/java/App.java
+++
@@ -1,9 +0,0 @@
-
-import core.LexerAnalyzer.LexerAnalyzer;
-
-public class App {
-    public static void main(String[] args) {
-        new LexerAnalyzer("src/main/resources/samples/sample1.txt");
-
-    }
-}
