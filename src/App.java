773f2b6be24e16a2ab92171276af2cd4adc31544
charles-braga-unvoid <165035397+charles-braga-unvoid@users.noreply.github.com>
2024-04-12 15:36:21 -0300

    refactor: modularizar a parte de selecionar arquivo do sample

--- src/App.java
+++ src/App.java
@@ -3,67 +3,15 @@
 
 import java.io.FileReader;
 import java.io.File;
-import java.util.Scanner;
 
+import core.Lexer.Lexer;
 import core.Lexer.Token;
-import core.Lexer.TOKEN_TYPE;
+import utils.SampleFileManager.SampleFileManager;
 
 public class App {
-  private static String selectFolder(File[] folders) {
-    System.out.println("Selecione um pasta:");
-    for (int i = 0; i < folders.length; i++) {
-      System.out.println((i + 1) + ". " + folders[i].getName());
-    }
-
-    Scanner scanner = new Scanner(System.in);
-    int choice = -1;
-
-    while (choice < 1 || choice > folders.length) {
-      System.out.print("Digite o número do pasta para selecionar: ");
-      choice = scanner.nextInt();
-    }
-    File selectedFolder = folders[choice - 1];
-    System.out.println("Você selecionou: " + selectedFolder.getPath());
-
-    return selectedFolder.getPath();
-  }
 
   public static void main(String[] args) throws Exception {
-    String directoryPath = "samples/sintatic/";
-
-    File directory_folder = new File(directoryPath);
-    File[] folders = directory_folder.listFiles();
-
-    if (folders != null || folders.length != 0) {
-      directoryPath = selectFolder(folders);
-    }
-
-    File directory = new File(directoryPath);
-    File[] files = directory.listFiles();
-
-    if (files == null || files.length == 0) {
-      System.out.println("Não há arquivos no diretório especificado.");
-      return;
-    }
-
-    // Exibir um menu com os arquivos disponíveis
-    System.out.println("Selecione um arquivo: (/samples/sintatic/)");
-    for (int i = 0; i < files.length; i++) {
-      System.out.println((i + 1) + ". " + files[i].getName());
-    }
-
-    // Ler a entrada do usuário para selecionar um arquivo
-    Scanner scanner = new Scanner(System.in);
-    int choice = -1;
-
-    while (choice < 1 || choice > files.length) {
-      System.out.print("Digite o número do arquivo para selecionar: ");
-      choice = scanner.nextInt();
-    }
-
-    // Processar o arquivo selecionado
-    File selectedFile = files[choice - 1];
-    System.out.println("Você selecionou: " + selectedFile.getPath());
+    File selectedFile = SampleFileManager.getSampleFile();
 
     Lexer lx = new Lexer(new FileReader(selectedFile));
     Token t = lx.nextToken();
---
+++ src/utils/SampleFileManager/SampleFileManager.java
@@ -0,0 +1,77 @@
+// Nome: Charles Lelis Braga - Matrícula: 202035015
+// Nome: Gabriella Carvalho -- Matrícula: 202165047AC
+
+package utils.SampleFileManager;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class SampleFileManager {
+  static String sampleFolder = "samples/sintatic/";
+
+  public static File getSampleFile() throws Exception {
+      File[] files = getFolders();
+      return selectFile(files);
+  }
+
+  private static File[] getFolders() throws Exception {
+    File directory_folder = new File(sampleFolder);
+    File[] folders = directory_folder.listFiles();
+
+    if (folders != null && folders.length != 0) {
+      sampleFolder = selectFolder(folders);
+    }
+
+    File directory = new File(sampleFolder);
+    File[] files = directory.listFiles();
+
+    if (files == null || files.length == 0) {
+      throw new Exception("Não há arquivos no diretório especificado.");
+    }
+
+    return files;
+  }
+
+  private static String selectFolder(File[] folders) {
+    System.out.println("Selecione um pasta:");
+
+    for (int i = 0; i < folders.length; i++) {
+      System.out.println((i + 1) + ". " + folders[i].getName());
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    int choice = -1;
+
+    while (choice < 1 || choice > folders.length) {
+      System.out.print("Digite o número do pasta para selecionar: ");
+      choice = scanner.nextInt();
+    }
+
+    File selectedFolder = folders[choice - 1];
+    System.out.println("Você selecionou: " + selectedFolder.getPath());
+
+    return selectedFolder.getPath();
+  }
+
+  private static File selectFile(File[] files)  {
+    System.out.println("Selecione um arquivo: (/samples/sintatic/)");
+
+    for (int i = 0; i < files.length; i++) {
+      System.out.println((i + 1) + ". " + files[i].getName());
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    int choice = -1;
+
+    while (choice < 1 || choice > files.length) {
+      System.out.print("Digite o número do arquivo para selecionar: ");
+      choice = scanner.nextInt();
+    }
+
+    File selectedFile = files[choice - 1];
+    System.out.println("Você selecionou: " + selectedFile.getPath());
+
+    return selectedFile;
+  }
+
+}
