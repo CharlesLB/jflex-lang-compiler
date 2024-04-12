612a1c59cecc55e56affcf73b3725dac6a7d8f25
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2024-04-12 09:47:13 -0300

    feat: Menu no APP

--- src/App.java
+++ src/App.java
@@ -12,8 +12,34 @@ import src.lexer.Token;
 import src.lexer.Lexer;
 
 public class App {
+  private static String selectFolder(File[] folders) {
+    System.out.println("Selecione um pasta:");
+    for (int i = 0; i < folders.length; i++) {
+      System.out.println((i + 1) + ". " + folders[i].getName());
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    int choice = -1;
+
+    while (choice < 1 || choice > folders.length) {
+      System.out.print("Digite o número do pasta para selecionar: ");
+      choice = scanner.nextInt();
+    }
+    File selectedFolder = folders[choice - 1];
+    System.out.println("Você selecionou: " + selectedFolder.getPath());
+
+    return selectedFolder.getPath();
+  }
+
   public static void main(String[] args) throws Exception {
-    String directoryPath = "samples/sintatic/false";
+    String directoryPath = "samples/sintatic/";
+
+    File directory_folder = new File(directoryPath);
+    File[] folders = directory_folder.listFiles();
+
+    if (folders != null || folders.length != 0) {
+      directoryPath = selectFolder(folders);
+    }
 
     File directory = new File(directoryPath);
     File[] files = directory.listFiles();
@@ -48,5 +74,7 @@ public class App {
       System.out.println(t.toString());
       t = lx.nextToken();
     }
+
   }
+
 }
