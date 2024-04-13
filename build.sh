56817bdb5db845f21686b1b7cf134ffd00ec1e17
CharlesLB <charles.braga.jf.cb@gmail.com>
2024-04-13 11:23:17 -0300

    fix: consertando o chuild.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 java -jar ./lib/jflex-full-1.8.2.jar ./src/core/Lexer/lang.flex
 
-javac -d bin src/**/*.java
+javac -d bin src/core/Lexer/Lexer.java src/core/Lexer/Token.java src/core/Lexer/TOKEN_TYPE.java src/core/Lexer/LexerProcessor.java  ./src/utils/SampleFileManager/SampleFileManager.java  src/App.java
 
 java -cp bin App
